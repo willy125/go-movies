9611ad932dc55b9bbcdece2b1dbc9f51860f7105
Jose Wilfrido Acuña Meraz <HSISTEMAS2@gastral.dom>
2023-02-10 13:05:48 -0700

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import EditMovie from './components/EditMovie';
+import ErrorPage from './components/ErrorPage';
+import Genres from './components/Genres';
+import GraphQL from './components/GraphQL';
+import Home from './components/Home';
+import Login from './components/Login';
+import ManageCatalogue from './components/ManageCatalogue';
+import Movies from './components/Movies';
+import Movie from './components/Movie';
+import OneGenere from './components/OneGenre';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home /> },
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+      {
+        path: "/movies/:id",
+        element: <Movie />,
+      },
+      {
+        path: "/genres",
+        element: <Genres />,
+      },
+      {
+        path: "/genres/:id",
+        element: <OneGenere/>,
+      },
+      {
+        path: "/admin/movie/0",
+        element: <EditMovie />,
+      },
+      {
+        path: "/admin/movie/:id",
+        element: <EditMovie />,
+      },
+      {
+        path: "/manage-catalogue",
+        element: <ManageCatalogue />,
+      },
+      {
+        path: "/graphql",
+        element: <GraphQL />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
